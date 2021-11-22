150b44fa02f122d60851aa182e6dbef959e06a8e
Winnie Lin <winnie50431@gmail.com>
2021-11-22 15:31:05 +0800

    test router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world.</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Homepage = () => {
+  return (
+    <div>
+      <header></header>
+      <nav>
+        <Navbar />
+      </nav>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Homepage;
